e7e8c39ccf677b46761a93237a897beb79c88b14
Tiago Faria Gouvêa <112782437+OTIGAS@users.noreply.github.com>
2023-09-20 08:39:16 -0300

    page login

---
+++ login-sosit/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import Login from './page/Login';
+
+export default function App(){
+
+  return (
+    <Login />
+  )
+}
+
+const styles = StyleSheet.create({});
